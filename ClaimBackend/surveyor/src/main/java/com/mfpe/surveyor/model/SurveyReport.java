ca160ff109d98f0bdb7d967204e3da1dda14db04
Ashishw2022 <ashishwilson2022a@mca.ajce.in>
2023-12-29 10:23:52 +0530

    updated

--- ClaimBackend/surveyor/src/main/java/com/mfpe/surveyor/model/SurveyReport.java
+++ ClaimBackend/surveyor/src/main/java/com/mfpe/surveyor/model/SurveyReport.java
@@ -4,10 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.Min;
-import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
